f0da447646f12138a6ec0753649273f45cdf9599
Tom the Bomb <thello522@gmail.com>
2026-06-29 16:03:59 -0400

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        max_area = 0
+
+        left, right = 0, len(heights) - 1
+
+        while left < right:
+            h = min(lh := heights[left], rh := heights[right])
+
+            area = h * (right - left)
+            max_area = max(max_area, area)
+            
+            # attempt to find a taller pole if current left shorter than current right
+            if h == lh:
+                left += 1
+            else:
+                right -= 1
+        return max_area
